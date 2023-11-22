19eadf0611605e8493ce89552d70420903aaf1ee
NicolasAvilaB <nicolasab1996@gmail.com>
2023-11-22 17:56:44 -0300

    added textfield pass

--- EventoVendimia/app/build.gradle.kts
+++ EventoVendimia/app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.material:material-icons-extended:1.1.1")
     implementation("com.google.code.gson:gson:2.8.7")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
